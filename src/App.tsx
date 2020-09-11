b3411c81ae2b64cc184d44fbee61d599e77fb27d
Willem Verbuyst <willemverbuyst@gmail.com>
2020-09-11 21:04:32 +0200

    Update ts to get bootstrap working

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Header from './header/Header';
 
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/header/Header.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+export default function Header() {
+  return <Container>This is a header</Container>;
+}
