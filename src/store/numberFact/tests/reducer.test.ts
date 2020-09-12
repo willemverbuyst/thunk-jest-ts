3738a1e08e664c5f9ba638905aa68452643a9c0f
Willem Verbuyst <willemverbuyst@gmail.com>
2020-09-12 15:06:31 +0200

    Test reducer numberFact

---
+++ src/store/numberFact/tests/reducer.test.ts
@@ -0,0 +1,18 @@
+import reducer from '../reducer';
+import { FACT_FETCHED, FactState } from '../types';
+
+describe('#fetchFact', () => {
+  const initialState: FactState = {
+    fact: null,
+  };
+  describe('with initial state and #FACT_FETCHED action', () => {
+    test('returns the new state with fact', () => {
+      const newState: FactState = reducer(initialState, {
+        type: FACT_FETCHED,
+        fact: 'test_fact',
+      });
+      expect(newState).toEqual({ fact: 'test_fact' });
+      expect(newState).not.toEqual(initialState);
+    });
+  });
+});
