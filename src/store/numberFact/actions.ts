8e54785b75c9530fbb5c35fe81a1d19488083dc7
Willem Verbuyst <willemverbuyst@gmail.com>
2020-09-12 14:17:54 +0200

    Use ThunkAction type in thunk

--- src/store/numberFact/actions.ts
+++ src/store/numberFact/actions.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-import { Dispatch } from 'redux';
 import { FACT_FETCHED, StoreNumber } from './types';
 import { GetState } from '../types';
 import {
@@ -8,6 +7,8 @@ import {
   appDoneLoading,
   showMessageWithTimeout,
 } from '../appState/actions';
+import { ThunkAction } from 'redux-thunk';
+import { Action } from 'redux';
 
 const factFetched = (fact: string): StoreNumber => {
   return {
@@ -16,16 +17,15 @@ const factFetched = (fact: string): StoreNumber => {
   };
 };
 
-export const fetchFact = (num: number) => async (
-  dispatch: Dispatch,
-  _getState: GetState
-) => {
+export const fetchFact = (
+  num: number
+): ThunkAction<void, GetState, unknown, Action<string>> => async (dispatch) => {
   dispatch(appLoading());
   try {
     const response = await axios.get(`http://numbersapi.com/${num}/trivia`);
 
     const fact = response.data;
-    dispatch<any>(showMessageWithTimeout(fact, 1500));
+    dispatch(showMessageWithTimeout(fact, 1500));
     dispatch(factFetched(fact));
 
     dispatch(appDoneLoading());
