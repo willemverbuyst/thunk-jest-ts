5d4bf4bd0b7a7b2dfb745037ae5017d100cc4df0
Willem Verbuyst <willemverbuyst@gmail.com>
2020-09-12 08:06:46 +0200

    Simplify header

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,21 +1,13 @@
 import React from 'react';
 
-import { Navbar, Nav, Form, Button } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
 
 export default function Header() {
   return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">THUNK JEST TYPESCRIPT</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-        </Nav>
-        <Form inline>
-          <Button variant="outline-success">Login</Button>
-        </Form>
-      </Navbar.Collapse>
+    <Navbar bg="light">
+      <Navbar.Brand href="#home">
+        REDUX &bull; THUNK &bull; JEST &bull; TYPESCRIPT
+      </Navbar.Brand>
     </Navbar>
   );
 }
