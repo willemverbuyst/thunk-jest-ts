5f06af3414524f3a31cf402aae2c045b2c3febea
Willem Verbuyst <willemverbuyst@gmail.com>
2020-09-12 08:20:56 +0200

    Control input with useState

--- src/question/Question.tsx
+++ src/question/Question.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Form, Button, Row, Col } from 'react-bootstrap';
 
 export default function Question() {
+  const [randomNumber, setRandomNumber] = useState<number>(0);
+
+  console.log(randomNumber);
+
   return (
     <Container>
       <Row>
@@ -9,7 +13,11 @@ export default function Question() {
           <Form>
             <Form.Group controlId="formNumber">
               <Form.Label>Enter a number</Form.Label>
-              <Form.Control type="number" />
+              <Form.Control
+                type="number"
+                value={randomNumber}
+                onChange={(e) => setRandomNumber(+e.target.value)}
+              />
               <Form.Text className="text-muted">
                 Enter a number, and you will get a random fact.
               </Form.Text>
