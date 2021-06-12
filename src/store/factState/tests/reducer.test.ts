264b939a06a9b4ffa609c10d5c6661559f1e37a8
Willem Verbuyst <willemverbuyst@gmail.com>
2021-06-12 07:30:24 +0200

    Test reducer factState

--- src/store/factState/tests/reducer.test.ts
+++ src/store/factState/tests/reducer.test.ts
@@ -1,18 +1,20 @@
-import { FACT_FETCHED, FactState } from '../action-types';
-import reducer from '../reducer';
+import { ActionType } from '../action-types';
+import reducer, { IFactState } from '../reducer';
 
-describe('#fetchFact', () => {
-  const initialState: FactState = {
-    fact: null,
-  };
-  describe('with initial state and #FACT_FETCHED action', () => {
-    test('returns the new state with fact', () => {
-      const newState: FactState = reducer(initialState, {
-        type: FACT_FETCHED,
-        fact: 'test_fact',
-      });
-      expect(newState).toEqual({ fact: 'test_fact' });
+describe('#factStateReducer', () => {
+  describe('w/ initial state and FACT_FETCHED action ', () => {
+    const initialState: IFactState = {
+      fact: null,
+    };
+    const newState: IFactState = reducer(undefined, {
+      type: ActionType.FACT_FETCHED,
+      payload: 'test',
+    });
+
+    test('returns a state with teachers', () => {
       expect(newState).not.toEqual(initialState);
+      expect(newState.fact).not.toBeNull();
+      expect(newState.fact).toEqual('test');
     });
   });
 });
